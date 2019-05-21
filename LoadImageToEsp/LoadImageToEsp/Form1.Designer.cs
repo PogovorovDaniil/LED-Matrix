eca589bf9ae08a0a77722cb423ffdb11d5988067
PogovorovDaniil <GigSter2000@yandex,ru>
2019-05-21 21:02:28 +0300

    /

---
+++ LoadImageToEsp/LoadImageToEsp/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace LoadImageToEsp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(240, 240);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "http://192.168.4.1/";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Отправить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 284);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(240, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Открыть изображение";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 345);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ LoadImageToEsp/LoadImageToEsp/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.Net;
+using System.Windows.Forms;
+
+namespace LoadImageToEsp
+{
+    public partial class Form1 : Form
+    {
+        static Bitmap bmp = new Bitmap(12, 12);
+        string path = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(path == null)
+                {
+                    MessageBox.Show("Изображение не загружено.");
+                    return;
+                }
+                string str = "";
+                path = path.Replace("\"", "");
+                bmp = new Bitmap(path);
+                bmp = new Bitmap(bmp, 12, 12);
+                int k = 0;
+                for (int i = 0; i < 12; i++)
+                {
+                    for (int j = 0; j < 12; j++)
+                    {
+                        str = str + bmp.GetPixel(i, j).R + " ";
+                        str = str + bmp.GetPixel(i, j).G + " ";
+                        str = str + bmp.GetPixel(i, j).B + " ";
+                        k += 3;
+                    }
+                }
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(textBox1.Text + "?str=" + str);
+                req.GetResponse();
+
+                MessageBox.Show("Изображение отправлено!");
+            }
+            catch (Exception ex) { /*MessageBox.Show("Произошла ошибка: " + ex);*/ }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Файлы изображений (*.bmp, *.jpg, *.png)|*.bmp;*.jpg;*.png";
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            path = openFileDialog1.FileName;
+            bmp = new Bitmap(path);
+            bmp = new Bitmap(bmp, 12, 12);
+            Bitmap tobox = new Bitmap(240, 240);
+            for (int y = 0; y < 12; y++)
+            {
+                for (int x = 0; x < 12; x++)
+                {
+                    for (int j = 0; j < 20; j++)
+                    {
+                        for (int i = 0; i < 20; i++)
+                        {
+                            tobox.SetPixel(20 * x + i, 20 * y + j, bmp.GetPixel(x, y));
+                        }
+                    }
+                }
+            }
+            pictureBox1.Image = tobox;
+        }
+    }
+}
